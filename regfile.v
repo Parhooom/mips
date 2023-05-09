f5e8e288748d173b5ae1545e29ef6d9ebd9efb58
Parham <alimohammadiparham@gmail.com>
2023-05-09 14:54:58 +0330

    reg file bug fixes

--- regfile.v
+++ regfile.v
@@ -11,26 +11,27 @@ module regfile (
     input regwrite;
     input [4:0] readreg1, readreg2, writereg;
     input [31:0] writedata;
-    output reg [31:0] readdata1, readdata2;
+    output [31:0] readdata1, readdata2;
 
     reg [31:0] file_reg [0:31];
 
     integer i;
+
     initial begin
         for (i = 0; i < 32; i = i + 1) begin // for testing
             file_reg[i] = i;
         end
     end
 
+
     always @(readreg1 or readreg2 or writereg
     or writedata or regwrite) begin
-        if (regwrite && writereg != 5'd0) begin
-            file_reg[writereg] = writedata;        
-        end else begin
-            readdata1 = file_reg[readreg1];
-            readdata2 = file_reg[readreg2];
-        end
+        if (regwrite == 1'b1 && writereg != 5'd0) 
+            file_reg[writereg] = writedata;
     end
+
+    assign readdata1 = (readreg1 == 5'd0) ? 32'd0: file_reg[readreg1];
+    assign readdata2 = (readreg2 == 5'd0) ? 32'd0: file_reg[readreg2];
 
 endmodule
 
@@ -61,7 +62,14 @@ module regfile_test;
         writedata = 32'd10;
 
         #30;
+        
+        regwrite = 1'b1;
+        readreg1 = 5'd5;
+        #30;
+
+        regwrite = 1'b0;
         $stop;
+
     end
     
 endmodule
